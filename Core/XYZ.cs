3a23cfbe988078bf8069192ea6fcbd850c3c5cfb
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2025-03-30 21:19:32 +0000

    Fix `XYZ.Rel` returning ~0 if input equals to 0

--- Core/XYZ.cs
+++ Core/XYZ.cs
@@ -6,7 +6,10 @@
         public static (string x, string y) Vec2(string x, string y) => (x, y);
         public static (int x, int y, int z) Vec3(int x, int y, int z) => (x, y, z);
         public static (string x, string y, string z) Vec3(string x, string y, string z) => (x, y, z);
-        public static (string x, string y, string z) Rel(int x, int y, int z) => ('~'+x.ToString(), '~'+y.ToString(), '~'+z.ToString());
+        public static (string x, string y, string z) Rel(int x, int y, int z)
+            => ('~' + (x == 0 ? "" : x.ToString()),
+                '~' + (y == 0 ? "" : y.ToString()),
+                '~' + (z == 0 ? "" : z.ToString()));
         public static string[] Pos(int a, int b, int c) {
             return ["^"+b, "^"+b, "^"+c];
         }
