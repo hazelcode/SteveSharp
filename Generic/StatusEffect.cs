9ad33c83b1fe387fe2d217ff00719fe69ac6d0c3
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2025-03-30 18:15:09 +0000

    Add `/effect` command (`Effect`) and implement `StatusEffect` enumeration

---
+++ Generic/StatusEffect.cs
@@ -0,0 +1,62 @@
+namespace SteveSharp.Generic;
+
+// https://www.digminecraft.com/lists/effect_list_pc.php
+public enum StatusEffect {
+    Absorption,
+    BadLuck,
+    BadOmen,
+    Blindness,
+    ConduitPower,
+    Darkness,
+    DolphinsGrace,
+    FireResistance,
+    Glowing,
+    Haste,
+    HealthBoost,
+    HeroOfTheVillage,
+    Hunger,
+    /// <summary>
+    /// Minecraft 1.21
+    /// </summary>
+    Infested,
+    InstantDamage,
+    InstantHealth,
+    Invisibility,
+    JumpBoost,
+    Levitation,
+    Luck,
+    MiningFatigue,
+    Nausea,
+    NightVision,
+    /// <summary>
+    /// Minecraft 1.21
+    /// </summary>
+    Oozing,
+    Poison,
+    /// <summary>
+    /// Minecraft 1.21
+    /// </summary>
+    RaidOmen,
+    Regeneration,
+    Resistance,
+    Saturation,
+    SlowFalling,
+    Slowness,
+    Speed,
+    Strength,
+    /// <summary>
+    /// Minecraft 1.21
+    /// </summary>
+    TrialOmen,
+    WaterBreathing,
+    Weakness,
+    /// <summary>
+    /// Minecraft 1.21
+    /// </summary>
+    Weaving,
+    /// <summary>
+    /// Minecraft 1.21
+    /// </summary>
+    WindCharged,
+    Wither
+}
---
+++ Generic/StatusEffectHandler.cs
@@ -0,0 +1,64 @@
+using SteveSharp.Exceptions;
+using SteveSharp.Internal;
+
+namespace SteveSharp.Generic;
+
+// https://www.digminecraft.com/lists/effect_list_pc.php
+public static class StatusEffectHandler {
+    public static string Get(StatusEffect effect) {
+        Dictionary<StatusEffect, string> statusEffectStr = new() {
+            {StatusEffect.Absorption, "absorption"},
+            {StatusEffect.BadLuck, "unluck"},
+            {StatusEffect.BadOmen, "bad_omen"},
+            {StatusEffect.Blindness, "blindness"},
+            {StatusEffect.ConduitPower, "conduit_power"},
+            {StatusEffect.Darkness, "darkness"},
+            {StatusEffect.DolphinsGrace, "dolphins_grace"},
+            {StatusEffect.FireResistance, "fire_resistance"},
+            {StatusEffect.Glowing, "glowing"},
+            {StatusEffect.Haste, "haste"},
+            {StatusEffect.HealthBoost, "health_boost"},
+            {StatusEffect.HeroOfTheVillage, "hero_of_the_village"},
+            {StatusEffect.Hunger, "hunger"},
+            {StatusEffect.Infested, "infested"},
+            {StatusEffect.InstantDamage, "instant_damage"},
+            {StatusEffect.InstantHealth, "instant_health"},
+            {StatusEffect.Invisibility, "invisibility"},
+            {StatusEffect.JumpBoost, "jump_boost"},
+            {StatusEffect.Levitation, "levitation"},
+            {StatusEffect.Luck, "luck"},
+            {StatusEffect.MiningFatigue, "mining_fatigue"},
+            {StatusEffect.Nausea, "nausea"},
+            {StatusEffect.NightVision, "night_vision"},
+            {StatusEffect.Oozing, "oozing"},
+            {StatusEffect.Poison, "poison"},
+            {StatusEffect.RaidOmen, "raid_omen"},
+            {StatusEffect.Regeneration, "regeneration"},
+            {StatusEffect.Resistance, "resistance"},
+            {StatusEffect.Saturation, "saturation"},
+            {StatusEffect.SlowFalling, "slow_falling"},
+            {StatusEffect.Slowness, "slowness"},
+            {StatusEffect.Speed, "speed"},
+            {StatusEffect.Strength, "strength"},
+            {StatusEffect.TrialOmen, "trial_omen"},
+            {StatusEffect.WaterBreathing, "water_breathing"},
+            {StatusEffect.Weakness, "weakness"},
+            {StatusEffect.Weaving, "weaving"},
+            {StatusEffect.WindCharged, "wind_charged"},
+            {StatusEffect.Wither, "wither"}
+        };
+        if(
+            Context.PackFormat < PackFormat.Format48
+            && (effect == StatusEffect.Infested
+            || effect == StatusEffect.Oozing
+            || effect == StatusEffect.RaidOmen
+            || effect == StatusEffect.TrialOmen
+            || effect == StatusEffect.Weaving
+            || effect == StatusEffect.WindCharged)
+        ) {
+            // Only available for Minecraft 1.21
+            throw new DatapackFeatureException(effect, PackFormat.Format48);
+        }
+        return statusEffectStr[effect];
+    }
+}
