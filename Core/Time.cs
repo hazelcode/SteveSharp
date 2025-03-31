ca44bb1de095698bb9326460373ad0025b7dcf93
Hazel Rojas <84425557+hazelcode@users.noreply.github.com>
2025-03-31 07:00:18 +0000

    Add `Time` for representing game time

---
+++ Core/Time.cs
@@ -0,0 +1,7 @@
+namespace SteveSharp.Core;
+
+public class Time {
+    public static string Ticks(int ticks) => ticks + "t";
+    public static string Seconds(int seconds) => seconds + "s";
+    public static string Days(int days) => days + "d";
+}
